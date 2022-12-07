6b617d5fbc9783064001aefeaf4d0f2e8a3e3681
David Amaku <amaku@Davids-MacBook-Pro.local>
2022-12-07 01:23:15 +0100

    styling

--- components/CardWrapper.js
+++ components/CardWrapper.js
@@ -4,13 +4,14 @@ import styled from "styled-components";
 
 // Define the CardWrapper component using the styled() function
 const CardWrapper = styled.div`
-  display: ;
+background-color: white;
   flex-direction: row;
   justify-content: space-between;
   margin: 20px 0;
 `;
 // Define the heading styles
 const Heading = styled.h3`
+font-color: black;
   font-size: 1.2em;
   margin: 10px 0;
 `;
